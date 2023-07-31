fbd4392d98bd44c569f372a233b5fbc6474ace87
mehedyh259 <mehedyh259@gmail.com>
2023-08-01 00:42:55 +0600

    first commit

---
+++ basic.js
@@ -0,0 +1,268 @@
+// console.log("Hello World !");
+// console.log(5)
+
+//variable 
+
+// var x = 5; 
+//  x = 10;
+
+// console.log(x)
+
+
+// variable naming 
+
+// valid name
+// var x = 5
+// var age = 25
+// var my_name = "mehedy"
+// var name1 = "dhfao"
+// var $hello = "dosf a"
+
+// invalid name
+// var 1hello ; 
+// var my-age ;
+// var if ;
+
+// data types
+// var age = 25; // number
+// var name = 'Mehedy' // string
+// var x = true; // boolean
+
+// var x = 25.4565;
+// var y = parseInt(x)
+
+// console.log(y);
+
+// var num = "15.36";
+// var num1 = parseFloat(num)
+// console.log(parseInt(num1))
+
+// var number = "25.43254";
+// console.log(typeof number)
+
+// var floating = parseFloat(number);
+// var integer = parseInt(floating);
+// var fixed = floating.toFixed(2)
+
+// console.log(floating)
+// console.log(integer)
+// console.log( parseFloat(fixed))
+
+
+
+// var x = 5;
+// var y = 10;
+// var z = x + y;
+
+// x = x + 5;
+// console.log(x)
+
+
+// console.log(z)
+
+// var x = 5;
+// x = x+1
+// x += 1
+// x++ // post increment
+// ++x // pre increment
+
+// var x = 5;
+// console.log(x++) // print hocce 5 tarpor bartece 1 = 6
+// console.log(x++) // print hocce 6 tarpor bartece 1 = 7
+
+// var y = 10;
+// console.log(++y)
+// console.log(++y)
+
+// array
+// var arr = [10,15,20,30]
+// console.log(arr.length)
+// var num1 = arr[0];
+
+// console.log(num1)
+
+// var hasan = ["sadia","nadia"]
+
+// hasan.push("jorina")
+// hasan.push("sokina")
+// hasan.push("sumaiya")
+
+// console.log(hasan)
+// var ex = hasan.pop()
+// hasan.pop()
+// hasan.push("daliya")
+// console.log(hasan)
+// console.log(ex)
+
+
+// are you adult or not? 
+// var age = 25;
+
+// if(age >= 18){
+//     console.log("you are adult")
+// }else{
+//     console.log("tumi bacca aco")
+// }
+
+// var mark = 57;
+
+// if(mark >= 80){
+//     console.log("you got A+")
+// }else if(mark >= 70){
+//     console.log("you got A")
+// }else if(mark >= 60){
+//     console.log("you got B")
+// }else if(mark>= 50){
+//     console.log("you got C")
+// }else{
+//     console.log("tumi fail.. next")
+// }
+
+// var temperature = 25;
+// var isRaining = false;
+
+// if(temperature > 30){
+//     console.log("It is a hot day");
+//     if(isRaining === true){
+//         console.log("but it is raining")
+//     }else{
+//         console.log("and it is not raining")
+//     }
+// }else{
+//     console.log("It is a cold day");
+//     if(isRaining === true){
+//         console.log("but it is raining")
+//     }else{
+//         console.log("and it is not raining")
+//     }
+// }
+
+
+// console.log("Um sorry... ")
+// console.log("Um sorry... ")
+// console.log("Um sorry... ")
+// console.log("Um sorry... ")
+// console.log("Um sorry... ")
+// console.log("Um sorry... ")
+// console.log("Um sorry... ")
+// console.log("Um sorry... ")
+// console.log("Um sorry... ")
+// console.log("Um sorry... ")
+
+// var i = 1;
+// while(i<=10){
+//     console.log("Um sorry... ",i);
+//     i = i + 1;
+// }
+
+// for(var x = 1; x<=10; x++){
+//     console.log("Um sorry... ",x);
+// }
+
+// var arr = [10,20,30,40,30,50];
+
+// console.log(arr.indexOf(40))
+
+// console.log(arr.indexOf(100))
+// var x = 0;
+// while(x<arr.length){
+//     console.log(arr[x]);
+//     x++
+// }
+
+// for(var i = 0; i < arr.length; i++ ){
+
+//     if(arr[i] === 30){
+//         break;
+//     }
+//     console.log(arr[i])
+// }
+// for(var i = 0; i < arr.length; i++ ){
+
+//     if(arr[i] === 30){
+//         continue;
+//     }
+//     console.log(arr[i])
+// }
+
+
+// var x = "5";
+
+// if(x === '5'){
+//     console.log("equal to 5")
+// }else{
+//     console.log("not equal to 5")
+// }
+
+// var raining = true;
+// var umbrella = false;
+
+// if(raining == true && umbrella == true){
+//     console.log('baire jabo')
+// }else{
+//     console.log('jabona baire')
+// }
+
+// var raining = false;
+// var umbrella = false;
+
+// if (raining === false || umbrella === true) {
+//     console.log('baire jabo')
+// } else {
+//     console.log('jabona baire')
+// }
+
+// var x = 9;
+// var y = 10;
+
+// var remain = x % y;
+
+// console.log(remain)
+
+// abul vai biye korbe, per bus 50 , per car 11 , badbaki jabe rikshaw per person 70
+
+// var peoples =176;
+
+// var peopleForCar = peoples % 50 ;
+// var peopleForRickshaw = peopleForCar % 11;
+
+// var totalVara = peopleForRickshaw * 70;
+// console.log(totalVara);
+
+
+
+// ternary operator 
+
+// var raining = true;
+
+// if(raining === true)
+//     console.log('baire jabona');
+// else
+//     console.log("baire jabo");
+
+
+// raining === true ? console.log('baire jabona') : console.log("baire jabo")
+
+// var age = 25; 
+// var hasLicence = false;
+
+// if(age >= 18){
+//     if(hasLicence === true){
+//         console.log("adult with licence")
+//     }else{
+//         console.log('adult but has no licence')
+//     }
+// }else{
+//     console.log("you are not adult")
+// }
+
+// age >= 18 ? hasLicence === true ? console.log("adult with licence") : console.log('adult but has no licence') : console.log("you are not adult")
+
+// 10 / 2 = 5 + 0
+// 10 / 3 = 3 + 1
+// 3 / 10 = 0 + 3
+// console.log(12 % 10)
+// "23"
+
+// console.log(parseInt("5") + 7)
+
